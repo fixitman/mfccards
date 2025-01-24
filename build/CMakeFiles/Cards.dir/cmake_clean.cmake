5c5e881e19200067e5c6f17ac0b326229e2ca644
Mike Collins <fixitman.mike@outlook.com>
2025-01-24 14:44:37 -0500

    commit 2

---
+++ build/CMakeFiles/Cards.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cards.dir/Card.cpp.obj"
+  "CMakeFiles/Cards.dir/Card.cpp.obj.d"
+  "Cards.exe"
+  "Cards.exe.manifest"
+  "Cards.pdb"
+  "libCards.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cards.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
